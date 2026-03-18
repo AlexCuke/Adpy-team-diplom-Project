b3e9c4caefa34bc47c7ee0c58acc59960f1c148b
AlexCuke <alexcuke@gmail.com>
2026-03-18 19:50:58 +0300

    Test token

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class VK:
+
+   def __init__(self, access_token, user_id, version='5.199'):
+       self.token = access_token
+       self.id = user_id
+       self.version = version
+       self.params = {'access_token': self.token, 'v': self.version}
+
+
+   def users_info(self):
+       url = 'https://api.vk.com/method/users.get'
+       params = {'user_ids': self.id}
+       response = requests.get(url, params={**self.params, **params})
+       return response.json()
+
+access_token = os.getenv('VK_GROUP_TOKEN')
+print(access_token)
+user_id = 'user_id'
+vk = VK(access_token, user_id)
+
+print(vk.users_info())
+
+
+
